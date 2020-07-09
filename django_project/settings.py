82ee2c8d1af396d34805ceb105439ea928ee009b
chagy <chagy_x_@hotmail.com>
2020-07-09 08:41:43 +0700

    Update crispy form

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
